8527cb169aa89a0594af37a105609fc05e8efc75
Jeremy German <jeremy.german@gmail.com>
2021-09-21 17:28:33 -0400

    Added org-roam, moved straight set up, upgrade org

--- init.el
+++ init.el
@@ -1,4 +1,34 @@
-(require 'org)
+;; loading straight in settings doesn't work because I want the latest version
+;; of org, and by the time I get to settings I've already required org so we
+;; will setup straight and require org here prior to the tangling the
+;; settings.org file
+
+
+;; early init handles disabling the built in package management
+;;
+
+;; bootstrap straight
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+;; we also need to pull in org mode now
+
+(use-package org :straight t)
+
+;;(require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
 		   user-emacs-directory))

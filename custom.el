5aa83b3dec559afe773b64e4f1c7bdacc6948f86
Jeremy German <jeremy.german@gmail.com>
2021-10-25 08:53:39 -0400

    Ace window, some python stuff

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(python-shell-interpreter "python3"))
+ '(python-shell-interpreter "python3")
+ '(safe-local-variable-values
+   '((cider-ns-refresh-after-fn . "integrant.repl/resume")
+     (cider-ns-refresh-before-fn . "integrant.repl/suspend"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
